3036855c3c62cb65fbd06f3fb71f18f115e043f4
DonCosme <109149406+franema@users.noreply.github.com>
2022-11-09 20:37:48 -0300

    Validates form inputs
    Validates the data in realtime, and prevents the form submit if there is an error

--- main.js
+++ main.js
@@ -1,40 +1,144 @@
-const $password = document.querySelector("#password")
-const $confirmPassword = document.querySelector("#confirm-password")
-const $inputs = document.querySelectorAll("input")
+const checkValidity = (() => {
 
-function checkMatch() {
+    const errors = {
+        "first-name": "error",
+        "last-name": "error",
+        "email": "error",
+        "password": "error",
+        "confirm-password": "error",
+    }
+
+    function showInvalidStyle(element) {
+        element.classList.add('invalid')
+    }
+
+    function removeInvalidStyle(element) {
+        element.classList.remove('invalid')
+    }
+
+    function manageErrors(element) {
+        const $errorMessage = document.querySelector(`.${element.id}`)
+        if (element.validity.valid) {
+            $errorMessage.style.opacity = "0"
+            removeInvalidStyle(element)
+            errors[element.id] = ""
+        } else {
+            $errorMessage.textContent = errors[element.id]
+            $errorMessage.style.opacity = "1"
+            showInvalidStyle(element)
+        }
+    }
+
+    return { errors, manageErrors }
+
+})()
+
+
+const validateName = (() => {
+
+    //DOM
+    const $firstName = document.querySelector("#first-name")
+    const $lastName = document.querySelector("#last-name")
+
+    //Bind Events
+    $firstName.addEventListener("input", showNameError)
+    $lastName.addEventListener("input", showNameError)
+
+    //Functions
+    function showNameError(e) {
+        if (e.target.validity.valueMissing) {
+            checkValidity.errors[e.target.id] = "Enter your name"
+        } else if (e.target.validity.patternMismatch) {
+            checkValidity.errors[e.target.id] = "Your name can only contain alphabetic characters"
+        } else if (e.target.validity.tooShort) {
+            checkValidity.errors[e.target.id] = "Your name must contain at least 2 characters"
+        } else if (e.target.validity.tooLong) {
+            checkValidity.errors[e.target.id] = "Your name must have less than 16 characters"
+        }
+
+        checkValidity.manageErrors(e.target)
+    }
+
+})()
+
+const validateEmail = (() => {
+
+    //DOM
+    const $email = document.querySelector("#email")
+
+    //Bind Events
+    $email.addEventListener("input", showEmailError)
+
+    //Functions
+    function showEmailError(e) {
+        if (e.target.validity.valueMissing) {
+            checkValidity.errors[e.target.id] = "Enter your email"
+        } else if (e.target.validity.typeMismatch) {
+            checkValidity.errors[e.target.id] = "Please enter a valid email"
+        }
+
+        checkValidity.manageErrors(e.target)
+    }
+
+})()
+
+const validatePassword = (() => {
+
+    //DOM
     const $password = document.querySelector("#password")
     const $confirmPassword = document.querySelector("#confirm-password")
-    if ($password.value !== $confirmPassword.value) {
-        const message = document.querySelector(".password p")
-        message.style.opacity = "1"
-        $confirmPassword.setCustomValidity("Passwords must match")
+
+    //Bind Events
+    $password.addEventListener("input", showPasswordError)
+    $confirmPassword.addEventListener("input", showConfirmPasswordError)
+
+    //Functions
+    function showPasswordError(e) {
+        if (e.target.validity.valueMissing) {
+            checkValidity.errors[e.target.id] = "Enter your password"
+        } else if (e.target.validity.tooShort) {
+            checkValidity.errors[e.target.id] = "Your password must contain at least 6 characters"
+        } else if (e.target.validity.tooLong) {
+            checkValidity.errors[e.target.id] = "Must be less than 16 characters"
+        }
+
+        checkValidity.manageErrors(e.target)
     }
-    if ($password.value === $confirmPassword.value) {
-        const message = document.querySelector(".password p")
-        message.style.opacity = "0"
-        $confirmPassword.setCustomValidity("")
+
+    function showConfirmPasswordError(e) {
+        if (e.target.validity.valueMissing) {
+            checkValidity.errors[e.target.id] = "Please confirm your password"
+        } else if ($password.value !== $confirmPassword.value) {
+            $confirmPassword.setCustomValidity("Passwords do not match")
+            checkValidity.errors[e.target.id] = "Passwords do not match"
+        } else {
+            $confirmPassword.setCustomValidity("")
+        }
+        checkValidity.manageErrors(e.target)
     }
-}
+})()
 
-function showInvalidInput () {
-    this.classList.add('invalid')
-}
+const validateForm = (() => {
 
-function removeInvalidStyle () {
-    this.classList.remove('invalid')
-}
+    //DOM
+    const $form = document.querySelector("form")
+    const $formErrorMessage = document.querySelector(".invalid-form")
 
+    //Bind Events
+    $form.addEventListener("submit", checkErrors)
 
-$inputs.forEach((input) => {
-    input.addEventListener("focus", showInvalidInput)
-})
+    //Functions
+    function checkErrors(e) {
+        if (checkValidity.errors["first-name"] !== "" ||
+            checkValidity.errors["last-name"] !== "" ||
+            checkValidity.errors["email"] !== "" ||
+            checkValidity.errors["password"] !== "" ||
+            checkValidity.errors["confirm-password"] !== ""
+        ) {
+            e.preventDefault()
+            $formErrorMessage.style.opacity = "1"
+        }
 
-$inputs.forEach((input) => {
-    input.addEventListener("input", removeInvalidStyle)
-})
+    }
 
-
-$password.addEventListener("input", checkMatch)
-$confirmPassword.addEventListener("input", checkMatch)
-
+})()
